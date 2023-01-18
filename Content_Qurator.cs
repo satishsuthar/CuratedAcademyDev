1445cb872c5bb1d98a33ae21f3c0ea2dbb64d066
Vineet Rai <105848287+RaiVineet@users.noreply.github.com>
2023-01-18 19:26:06 +1100

    Add files via upload

---
+++ Content_Qurator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+// model
+namespace CuratedAcademy.WebAPI.Models
+{
+	//pakages
+	public class Pakages
+	{
+		public int id { get; set; }
+		public string name { get; set; } = string.Empty;
+		public string? description { get; set; }
+		
+	}
+	// curators
+	public class Curators
+	{
+		public int id { get; set; }
+		public string Name { get; set; } = string.Empty;
+		public string UserName { get; set; } = string.Empty;
+		public string PreferredName { get; set; } = string.Empty;
+		public string Emailaddress { get; set; } = string.Empty;
+
+	}
+	// resources
+	public class Resources
+	{
+		public int id { get; set; }
+		public string BlogVedios { get; set; } = string.Empty;
+		public string Vediolinks { get; set; } = string.Empty;
+	}
+}
+
---
+++ Curator_Controller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc; // controller
+
+namespace CuratedAcademy.WebAPI.Controllers
+{
+	[ApiController]
+    [Route("api/Curator")]  // using the curator
+    public class Curator_Controller : ControllerBase
+	{
+        [HttpGet]
+        public JsonResult Curators()
+        {
+            return new JsonResult(CuratorData.Current.curators);
+        }
+        // get by the id
+        [HttpGet("{id}")]
+        public JsonResult OneCurators(int id)
+        {
+            return new JsonResult(CuratorData.Current.curators.FirstOrDefault(c => c.id == id));
+        }
+
+    }
+}
+
